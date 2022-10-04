5c623e23534c94c75c57db005b1e333b23882307
Benoit-Catta <csupremenight@gmail.com>
2022-10-04 17:15:38 +0200

    gros commit movie presque fini

--- client/client.py
+++ client/client.py
@@ -4,7 +4,7 @@ import movie_pb2
 import movie_pb2_grpc
 
 
-def get_movie_by_id(stub, id):
+def get_movie_by_id(stub,id):
     movie = stub.GetMovieByID(id)
     print(movie)
 
@@ -12,6 +12,25 @@ def get_list_movies(stub):
     allmovies = stub.GetListMovies(movie_pb2.Empty())
     for movie in allmovies:
         print("Movie called %s" % (movie.title))
+
+def GetMovieByTitle(stub, title):
+    movie = stub.GetMovieByTitle(title)
+    print(movie)
+
+def GetMovieByRating(stub, rating):
+    movie = stub.GetMovieByRating(rating)
+    print(movie)
+
+def PostMovie(stub, movie):
+    movie = stub.PostMovie(movie)
+    print(movie)
+
+def PutRateByMovieId(stub, movieRateById):
+    movie = stub.PutRateByMovieId(movieRateById)
+    print(movie)
+
+def DeleteByMovieId(self, request, context):
+    raise NotImplementedError('Method not implemented!')
 
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
@@ -26,6 +45,22 @@ def run():
 
         print("-------------- GetListMovies --------------")
         get_list_movies(stub)
+
+        print("---------GetMovieByTitle-------------")
+        title = movie_pb2.MovieTitle(title="The Good Dinosaur")
+        GetMovieByTitle(stub, title)
+
+        print("---------GetMovieByRating-------------")
+        rating = movie_pb2.MovieRating(rating=7.4)
+        GetMovieByRating(stub, rating)
+
+        print("---------PostMovie-------------")
+        movie = movie_pb2.MovieData(title="MonNouveauFilm",director="Moi Forcement",rating=10.0,id="id")
+        PostMovie(stub, movie)
+
+        print("---------PutRateByMovieId-------------")
+        movie = movie_pb2.MovieIdRating(id="id", rating=0.0)
+        PutRateByMovieId(stub, movie)
 
     channel.close()
 
--- movie/movie.py
+++ movie/movie.py
@@ -4,6 +4,7 @@ import movie_pb2
 import movie_pb2_grpc
 import json
 
+
 class MovieServicer(movie_pb2_grpc.MovieServicer):
 
     def __init__(self):
@@ -14,13 +15,49 @@ class MovieServicer(movie_pb2_grpc.MovieServicer):
         for movie in self.db:
             if movie['id'] == request.id:
                 print("Movie found!")
-                return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'], id=movie['id'])
+                return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
+                                           id=movie['id'])
         return movie_pb2.MovieData(title="", rating=0.0, director="", id="")
 
     def GetListMovies(self, request, context):
         for movie in self.db:
             yield movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
                                       id=movie['id'])
+
+    def GetMovieByTitle(self, request, context):
+        for movie in self.db:
+            if movie['title'] == request.title:
+                return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
+                                           id=movie['id'])
+        return movie_pb2.MovieData(title="", rating=0.0, director="",
+                                   id="")
+
+    def GetMovieByRating(self, request, context):
+        for movie in self.db:
+            print(movie['rating'], request.rating, movie['rating'] - request.rating < 0.0)
+            if movie['rating'] - request.rating < 0.0:
+                print("Rating found")
+                return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
+                                           id=movie['id'])
+        return movie_pb2.MovieData(title="", rating=0.0, director="",
+                                   id="")
+
+    def PostMovie(self, request, context):
+        self.db.append({"title":request.title,"rating":request.rating,"director":request.director,"id":request.id})
+        return self.GetMovieByID(request,context)
+
+    def PutRateByMovieId(self, request, context):
+        for movie in self.db:
+            if movie['id'] == request.id:
+                movie['rating'] = request.rating + 0.01
+        return self.GetMovieByID(request, context)
+
+    def DeleteByMovieId(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
